6aaccb2648a4e58f240846881f15640f98b82808
ErinilsonCFS <erinilson.cfs@gmail.com>
2021-08-10 20:38:27 -0300

    Inicio do projeto

---
+++ src/test/java/br/com/geekstore/ecommerce/GeekstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geekstore.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
